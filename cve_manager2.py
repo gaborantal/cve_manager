27224f481c79e93ff51e2b86025c778220f4acc1
Gábor Antal <antal@inf.u-szeged.hu>
2020-02-29 15:11:02 +0100

    Added requirements.txt, moved to python3

--- cve_manager2.py
+++ cve_manager2.py
@@ -16,7 +16,7 @@ import zipfile
 import json
 import requests
 import re
-import StringIO
+import io
 
 ##This is the Postgresql Database Schema##
 query = '''
@@ -104,14 +104,14 @@ def create_database(myuser,mypassword,myhost,database, owner):
         con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         cur = con.cursor()
         cur.execute('CREATE DATABASE ' + dbname)
-        print "Database",database,"was created."
+        print("Database",database,"was created.")
         cur = con.cursor()
         query = '''ALTER DATABASE '''+ database + ''' OWNER TO ''' + owner+''';'''
-        print "Owner of the database changed to:",owner 
+        print("Owner of the database changed to:",owner) 
         cur.execute(query)
         con.commit()
     except (Exception, psycopg2.DatabaseError) as error :
-        print ("Error while creating PostgreSQL Database", error)
+        print(("Error while creating PostgreSQL Database", error))
     finally:
         #closing database connection.
         if(con):
@@ -126,9 +126,9 @@ def drop_database(myuser,mypassword,myhost,database):
         con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         cur = con.cursor()
         cur.execute('DROP DATABASE ' + database)
-        print "Database",database,"was dropped."
+        print("Database",database,"was dropped.")
     except (Exception, psycopg2.DatabaseError) as error :
-        print ("Error while dropping PostgreSQL Database", error)
+        print(("Error while dropping PostgreSQL Database", error))
     finally:
         #closing database connection.
         if(con):
@@ -145,9 +145,9 @@ def create_tables(myuser,mypassword,myhost,database):
 
         cursor.execute(create_tables_query)
         connection.commit()
-        print("Tables and Views created successfully for database: "+database)
+        print(("Tables and Views created successfully for database: "+database))
     except (Exception, psycopg2.DatabaseError) as error :
-        print ("Error while creating PostgreSQL tables", error)
+        print(("Error while creating PostgreSQL tables", error))
     finally:
         #closing database connection.
         if(connection):
@@ -160,15 +160,15 @@ def download_cves(directory,year):
         try:
             os.makedirs(directory)
         except OSError:
-            print ('Error: Creating directory. ' + directory)
+            print(('Error: Creating directory. ' + directory))
             exit(0)
         else:  
-            print ("Successfully created the directory %s" % directory)
+            print(("Successfully created the directory %s" % directory))
     else:
-        print ("Directory %s already exists" % directory)
+        print(("Directory %s already exists" % directory))
     r = requests.get('https://nvd.nist.gov/vuln/data-feeds#JSON_FEED')
     if year:
-        print("downloading ",year," only")
+        print(("downloading ",year," only"))
         filename = "nvdcve-1.1-"+year+".json.zip"
         print(filename)
         r_file = requests.get("https://nvd.nist.gov/feeds/json/cve/1.1/" + filename, stream=True)
@@ -189,16 +189,16 @@ def process_cves(directory, results, csv, import_db,myuser,mypassword,myhost,dat
             try:
                 os.makedirs(results)
             except OSError:
-                print ('Error: Creating directory. ' + results)
+                print(('Error: Creating directory. ' + results))
                 exit(0)
             else:  
-                print ("Successfully created the directory %s" % results)
+                print(("Successfully created the directory %s" % results))
         else:
-            print ("Directory %s already exists" % results)
-        file_cve_related_problems = open(results+"cve_related_problems.csv","w")
-        file_cvss_score = open(results+"cve_cvss_scores.csv","w")
-        file_cpes = open(results+"cve_cpes.csv","w")
-        file_cve_related_problems.write("CVE\tProblem\n")
+            print(("Directory %s already exists" % results))
+        file_cve_related_problems = open(results+"cve_related_problems.csv","w", encoding="utf-8")
+        file_cvss_score = open(results+"cve_cvss_scores.csv","w", encoding="utf-8")
+        file_cpes = open(results+"cve_cpes.csv","w", encoding="utf-8")
+        file_cve_related_problems.write("cve_data\tcwe_group\tpublished_date\tseverity\timpact_score\n")
         file_cpes.write("CVE\tcpe22Uri\tcpe23Uri\tVulnerable\n")
         file_cvss_score.write("CVE\tAttack Complexity\tAttack Vector\tAvailability Impact\tConfidentiality Impact\tIntegrity Impact\tPrivileges Required\tScope\tUserInteraction\tVector String\tExploitability Score\tImpact Score\tbase Score\tbase Severity\tAccess Complexity\tAccess Vector\tAuthentication\tAvailability Impact\tConfidentiality Impact\tIntegrityImpact\tObtain All Privilege\tobtain Other Privilege\tobtain User Privilege\tuser Interaction Required\tvector String\tExploitability Score\timpact Score\tbaseScore\tseverity\tDescription\tPublished Date\tLast Modified Date\n")
         ########################################################################################
@@ -207,15 +207,15 @@ def process_cves(directory, results, csv, import_db,myuser,mypassword,myhost,dat
         files = [f for f in listdir(directory) if isfile(join(directory, f))]
         files.sort(reverse=True)
         for file in files:
-            print "\nProcessing", file
+            print("\nProcessing", file)
             archive = zipfile.ZipFile(join(directory, file), 'r')
             jsonfile = archive.open(archive.namelist()[0])
             cve_dict = json.loads(jsonfile.read())
-            print("CVE_data_timestamp: " + str(cve_dict['CVE_data_timestamp']))
-            print("CVE_data_version: " + str(cve_dict['CVE_data_version']))
-            print("CVE_data_format: " + str(cve_dict['CVE_data_format']))
-            print("CVE_data_number of CVEs: " + str(cve_dict['CVE_data_numberOfCVEs']))
-            print("CVE_data_type: " + str(cve_dict['CVE_data_type']))
+            print(("CVE_data_timestamp: " + str(cve_dict['CVE_data_timestamp'])))
+            print(("CVE_data_version: " + str(cve_dict['CVE_data_version'])))
+            print(("CVE_data_format: " + str(cve_dict['CVE_data_format'])))
+            print(("CVE_data_number of CVEs: " + str(cve_dict['CVE_data_numberOfCVEs'])))
+            print(("CVE_data_type: " + str(cve_dict['CVE_data_type'])))
             all_cves = all_cves + cve_dict['CVE_Items']
             #print(json.dumps(cve_dict['CVE_Items'][0], sort_keys=True, indent=4, separators=(',', ': ')))
             jsonfile.close()
@@ -236,32 +236,49 @@ def process_cves(directory, results, csv, import_db,myuser,mypassword,myhost,dat
                 cvssv2_missing=False
                 try:
                     cvssv3 = separator.join([cves['impact']['baseMetricV3']['cvssV3']['attackComplexity'],cves['impact']['baseMetricV3']['cvssV3']['attackVector'],cves['impact']['baseMetricV3']['cvssV3']['availabilityImpact'],cves['impact']['baseMetricV3']['cvssV3']['confidentialityImpact'],cves['impact']['baseMetricV3']['cvssV3']['integrityImpact'],cves['impact']['baseMetricV3']['cvssV3']['privilegesRequired'],cves['impact']['baseMetricV3']['cvssV3']['scope'],cves['impact']['baseMetricV3']['cvssV3']['userInteraction'],cves['impact']['baseMetricV3']['cvssV3']['vectorString'],str(cves['impact']['baseMetricV3']['exploitabilityScore']),str(cves['impact']['baseMetricV3']['impactScore']),str(cves['impact']['baseMetricV3']['cvssV3']['baseScore']),str(cves['impact']['baseMetricV3']['cvssV3']['baseSeverity'])])
-                except Exception, e:
+                except Exception as e:
                     if str(e) == "'baseMetricV3'":
                         cvssv3_missing=True
                         cvssv3 = separator.join(["","","","","","","","","","","","",""])
                     else:
-                        print str(e)
+                        print(str(e))
                 try:
                     cvssv2 = separator.join([cves['impact']['baseMetricV2']['cvssV2']['accessComplexity'],cves['impact']['baseMetricV2']['cvssV2']['accessVector'],cves['impact']['baseMetricV2']['cvssV2']['authentication'],cves['impact']['baseMetricV2']['cvssV2']['availabilityImpact'],cves['impact']['baseMetricV2']['cvssV2']['confidentialityImpact'],cves['impact']['baseMetricV2']['cvssV2']['integrityImpact'],str(cves['impact']['baseMetricV2']['obtainAllPrivilege']),str(cves['impact']['baseMetricV2']['obtainOtherPrivilege']),str(cves['impact']['baseMetricV2']['obtainUserPrivilege']),str(cves['impact']['baseMetricV2']['userInteractionRequired']),cves['impact']['baseMetricV2']['cvssV2']['vectorString'],str(cves['impact']['baseMetricV2']['exploitabilityScore']),str(cves['impact']['baseMetricV2']['impactScore']),str(cves['impact']['baseMetricV2']['cvssV2']['baseScore']),str(cves['impact']['baseMetricV2']['severity'])])
-                except Exception, e:
+                except Exception as e:
                     if str(e) == "'baseMetricV2'":
                         cvssv2 = separator.join(["","","","","","","","","","","","",""])
                         cvssv2_missing=True
                     elif str(e) == "'userInteractionRequired'":
                         cvssv2 = separator.join([cves['impact']['baseMetricV2']['cvssV2']['accessComplexity'],cves['impact']['baseMetricV2']['cvssV2']['accessVector'],cves['impact']['baseMetricV2']['cvssV2']['authentication'],cves['impact']['baseMetricV2']['cvssV2']['availabilityImpact'],cves['impact']['baseMetricV2']['cvssV2']['confidentialityImpact'],cves['impact']['baseMetricV2']['cvssV2']['integrityImpact'],str(cves['impact']['baseMetricV2']['obtainAllPrivilege']),str(cves['impact']['baseMetricV2']['obtainOtherPrivilege']),str(cves['impact']['baseMetricV2']['obtainUserPrivilege']),"",cves['impact']['baseMetricV2']['cvssV2']['vectorString'],str(cves['impact']['baseMetricV2']['exploitabilityScore']),str(cves['impact']['baseMetricV2']['impactScore']),str(cves['impact']['baseMetricV2']['cvssV2']['baseScore']),str(cves['impact']['baseMetricV2']['severity'])])
                     else:
-                        print str(e)
+                        print(str(e))
                 if cvssv2_missing and cvssv3_missing:
                     #print "Both CVSSv2 and CVSSv3 are missing"
-                    file_cvss_score.write(cve.encode('utf-8')+"\t"+cvssv3+"\t"+cvssv2.encode('utf-8')+"\t"+"\t"+"\t"+description.encode('utf-8')+"\t"+cves['publishedDate'].encode('utf-8')+"\t"+cves['lastModifiedDate'].encode('utf-8')+"\n")
+                    file_cvss_score.write(f"{cve}\t{cvssv3}\t{cvssv2}\t\t\t{description}\t{cves['publishedDate']}\t{cves['lastModifiedDate']}\n")
+                    # file_cvss_score.write(f"{cve.encode('utf-8')}\t{cvssv3}\t{cvssv2.encode('utf-8')}\t\t\t{description.encode('utf-8')}\t{cves['publishedDate'].encode('utf-8')}\t{cves['lastModifiedDate'].encode('utf-8')}\n")
+                    # file_cvss_score.write(cve.encode('utf-8')+"\t"+cvssv3+"\t"+cvssv2.encode('utf-8')+"\t"+"\t"+"\t"+description.encode('utf-8')+"\t"+cves['publishedDate'].encode('utf-8')+"\t"+cves['lastModifiedDate'].encode('utf-8')+"\n")
                 else:
-                    file_cvss_score.write(cve.encode('utf-8')+"\t"+cvssv3.encode('utf-8')+"\t"+cvssv2.encode('utf-8')+"\t"+description.encode('utf-8')+"\t"+cves['publishedDate'].encode('utf-8')+"\t"+cves['lastModifiedDate'].encode('utf-8')+"\n")
+                    file_cvss_score.write(f"{cve}\t{cvssv3}\t{cvssv2}\t{description}\t{cves['publishedDate']}\t{cves['lastModifiedDate']}\n")
+                    # file_cvss_score.write(f"{cve.encode('utf-8')}\t{cvssv3.encode('utf-8')}\t{cvssv2.encode('utf-8')}\t{description.encode('utf-8')}\t{cves['publishedDate'].encode('utf-8')}\t{cves['lastModifiedDate'].encode('utf-8')}\n")
+                    # file_cvss_score.write(cve.encode('utf-8')+"\t"+cvssv3.encode('utf-8')+"\t"+cvssv2.encode('utf-8')+"\t"+description.encode('utf-8')+"\t"+cves['publishedDate'].encode('utf-8')+"\t"+cves['lastModifiedDate'].encode('utf-8')+"\n")
             for problem_type in cves['cve']['problemtype']['problemtype_data']:
                 for descr in problem_type['description']:
                     problem =  descr['value']
                     if csv:
-                        file_cve_related_problems.write(cve+"\t"+problem+"\n")
+                        # file_cve_related_problems.write(cve+"\t"+problem+"\n")
+                        #print(cvssv3_missing, cvssv2_missing, cves['impact'])
+                        file_cve_related_problems.write(f"{cve}\t{problem}\t{cves['publishedDate']}")
+                        if cves['impact']:
+                            if cves['impact'].get('baseMetricV2'): 
+                                file_cve_related_problems.write(f"\t{cves['impact']['baseMetricV2']['severity']}\t{cves['impact']['baseMetricV2']['impactScore']}\n")
+                            elif cves['impact'].get('baseMetricV3'):
+                                file_cve_related_problems.write(f"\t{cves['impact']['baseMetricV3']['cvssV3']['baseSeverity']}\t{cves['impact']['baseMetricV3']['impactScore']}\n")
+                            else:
+                                print("baseMetricV3 NO baseMetricV2")
+                                file_cve_related_problems.write("\t\t\n")
+                        else:
+                            print("No impact on cve: ", cve)
+                            file_cve_related_problems.write("\t\t\n")
             try:
                 cpe_list_length=len(cves['configurations']['nodes'])
                 if (cpe_list_length !=0):
@@ -299,8 +316,8 @@ def process_cves(directory, results, csv, import_db,myuser,mypassword,myhost,dat
                                                         file_cpes.write(cve+"\t"+cpe['cpe22Uri']+"\t"+cpe['cpe23Uri'].replace('cpe:2.3:o:','')+"\t"+str(cpe['vulnerable'])+"\n")
                                                     if 'cpe23Uri' in cpe:
                                                         file_cpes.write(cve+"\t"+"\t"+cpe['cpe23Uri']+"\t"+str(cpe['vulnerable'])+"\n")
-            except Exception, e:
-                print(str(e),cves['configurations']) #check it
+            except Exception as e:
+                print((str(e),cves['configurations'])) #check it
         file_cve_related_problems.close()
         file_cvss_score.close()
         file_cpes.close()
@@ -309,8 +326,8 @@ def process_cves(directory, results, csv, import_db,myuser,mypassword,myhost,dat
         try:
             conn = psycopg2.connect("dbname='"+database+"' user='"+myuser+"' host='"+myhost+"' password='"+mypassword+"'")
         except psycopg2.Error as e:
-            print "I am unable to connect to the database. Error:",e
-            print "Exiting"
+            print("I am unable to connect to the database. Error:",e)
+            print("Exiting")
             sys.exit(1)
         cur = conn.cursor()
         filename = results+"cve_cvss_scores.csv"
@@ -318,7 +335,7 @@ def process_cves(directory, results, csv, import_db,myuser,mypassword,myhost,dat
             print("importing CVSS")
             filedata = f.read()
             filedata = filedata.replace("\\","\\\\")
-            output = StringIO.StringIO()
+            output = io.StringIO()
             output.write(filedata)
             output.seek(0)
             output.readline()
@@ -346,11 +363,11 @@ def truncate_database(myuser,mypassword,myhost,database):
         con = connect(dbname=database, user=myuser, host = myhost, password=mypassword)
         con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         cur = con.cursor()
-        print "Truncating CVEs tables"
+        print("Truncating CVEs tables")
         cur.execute("Truncate cpe, cve_problem, cvss;")
         con.commit()
     except (Exception, psycopg2.DatabaseError) as error :
-        print ("Error while Truncating PostgreSQL Database", error)
+        print(("Error while Truncating PostgreSQL Database", error))
     finally:
         if(con):
             cur.close()
@@ -363,7 +380,7 @@ def execute_query(myuser,mypassword,myhost,database,cve,score,date):
         con = connect(dbname=database, user=myuser, host = myhost, password=mypassword)
         #con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         cur = con.cursor()
-        print "Executing query"
+        print("Executing query")
         if cve:
             cur.execute("SELECT cve, vector_string_3, base_score_3, base_severity_3, vector_string, base_score, severity, description, published_date FROM cvss WHERE cve LIKE '%"+cve+"%'")
             selected_cve = cur.fetchone()
@@ -374,9 +391,9 @@ def execute_query(myuser,mypassword,myhost,database,cve,score,date):
                 else:
                     answer = answer+str(r)+"\t"
             answer = answer.rstrip('\t')
-            print answer
+            print(answer)
     except (Exception, psycopg2.DatabaseError) as error :
-        print ("Error while Querying Database", error)
+        print(("Error while Querying Database", error))
     finally:
         if(con):
             cur.close()
@@ -384,7 +401,7 @@ def execute_query(myuser,mypassword,myhost,database,cve,score,date):
             print("PostgreSQL connection is closed")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(version='0.1',description='CVEs Manager.')
+    parser = argparse.ArgumentParser(description='CVEs Manager.') # version='0.1'
     parser.add_argument('-p', '--parse',  action="store_true", dest="process", default=False, help="Process downloaded CVEs.")
     parser.add_argument('-d', '--download',  action="store_true", dest="download", default=False, help="Download CVEs.")
     parser.add_argument('-y', '--year',  action="store", dest="year", default=False, help="The year for which CVEs shall be downloaded (e.g. 2019)")
@@ -423,4 +440,4 @@ if __name__ == '__main__':
     if values.cve:
         execute_query(values.user,values.password,values.host,values.database,values.cve,values.score,values.date)
     if not values.input and not values.process and not values.dd and not values.cd and not values.ct and not values.download and not values.process and not values.tr and not values.cve:
-        print "Choose an option (check --help)"
+        print("Choose an option (check --help)")
